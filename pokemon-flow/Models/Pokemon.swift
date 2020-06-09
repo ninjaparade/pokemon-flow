8921fee424d890d133bcf8ae18c62d97442aa923
Yaz Jallad <yazjallad@gmail.com>
2020-06-08 19:45:45 -0700

    pokemon model

--- pokemon-flow/Models/Pokemon.swift
+++ pokemon-flow/Models/Pokemon.swift
@@ -7,3 +7,104 @@
 //
 
 import Foundation
+
+struct PokemonSpecies: Codable {
+    let abilities: [Ability]
+    let baseExperience: Int?
+    let forms: [Species]
+    let height: Int
+    let id: Int
+    let moves: [Move]
+    let name: String
+    let order: Int
+    let species: Species
+    let sprites: Sprites
+    let stats: [Stat]
+    let types: [TypeElement]
+    let weight: Int
+}
+
+
+// MARK: - Ability
+struct Ability: Codable {
+    let ability: Species
+    let isHidden: Bool
+    let slot: Int
+
+    enum CodingKeys: String, CodingKey {
+        case ability
+        case isHidden = "is_hidden"
+        case slot
+    }
+}
+
+// MARK: - Species
+struct Species: Codable {
+    let name: String
+    let url: String
+}
+
+
+// MARK: - Move
+struct Move: Codable {
+    let move: Species
+    let versionGroupDetails: [VersionGroupDetail]
+
+    enum CodingKeys: String, CodingKey {
+        case move
+        case versionGroupDetails = "version_group_details"
+    }
+}
+
+// MARK: - VersionGroupDetail
+struct VersionGroupDetail: Codable {
+    let levelLearnedAt: Int
+    let moveLearnMethod, versionGroup: Species
+
+    enum CodingKeys: String, CodingKey {
+        case levelLearnedAt = "level_learned_at"
+        case moveLearnMethod = "move_learn_method"
+        case versionGroup = "version_group"
+    }
+}
+
+// MARK: - Sprites
+struct Sprites: Codable {
+    let backDefault: String
+    let backFemale: String?
+    let backShiny: String
+    let backShinyFemale: String?
+    let frontDefault: String
+    let frontFemale: String?
+    let frontShiny: String
+    let frontShinyFemale: String?
+
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case backFemale = "back_female"
+        case backShiny = "back_shiny"
+        case backShinyFemale = "back_shiny_female"
+        case frontDefault = "front_default"
+        case frontFemale = "front_female"
+        case frontShiny = "front_shiny"
+        case frontShinyFemale = "front_shiny_female"
+    }
+}
+
+// MARK: - Stat
+struct Stat: Codable {
+    let baseStat, effort: Int
+    let stat: Species
+
+    enum CodingKeys: String, CodingKey {
+        case baseStat = "base_stat"
+        case effort, stat
+    }
+}
+
+// MARK: - TypeElement
+struct TypeElement: Codable {
+    let slot: Int
+    let type: Species
+}
+
